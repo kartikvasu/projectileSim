fb21c5c9e7a4edfa7d73a6dda4879fb7eda56ed8
Kartik <kartiktv97@gmail.com>
2016-01-05 21:23:05 -0800

    Added bounce

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var w = window.innerWidth;
 var h = window.innerHeight;
 var updateInterval = 50;
+var verticalVelocity = -20;
 
 $(document).ready(function(){
   var canvas = $('#canvas');
@@ -19,18 +20,29 @@ $(document).ready(function(){
   setInterval(accelerateBall, updateInterval);
 });
 
-var verticalVelocity = -20;
-
 var accelerateBall = function(){
   verticalVelocity = verticalVelocity + 0.5;
 }
 
 //function that changes the css position of the ball every "updateInterval" milliseconds
 var animateBall = function(){
+
+  //bounce the ball if it reaches the bottom of the screen
+  if(h - findCurHeight() <= 0.1 * h){
+    verticalVelocity = -20;
+  }
+
   $('#ball').css({
     //number of pixels the ball moves horizontally every 50 miliseconds
     "margin-left": '+=10px',
     //number of pixels the ball moves vertically every 50 miliseconds
     "margin-top": '+=' + verticalVelocity + 'px'
   });
-};
+}
+
+var findCurHeight = function(){
+  var height = $('#ball').css("margin-top");
+  length = height.length;
+  height = height.slice(0, length - 2);
+  return height;
+}
