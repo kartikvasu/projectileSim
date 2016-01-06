dabbc091ef9b32f38246d26553b73502697c7036
Kartik <kartiktv97@gmail.com>
2016-01-05 18:50:05 -0800

    added colors

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var w = window.innerWidth;
 var h = window.innerHeight;
+var updateInterval = 50;
 
 $(document).ready(function(){
   var canvas = $('#canvas');
-  //Creation of the ball element
 
+  //Creation of the ball element
   canvas.append("<div id = 'ball'></div>");
 
   var ball = $('#ball');
@@ -14,8 +15,8 @@ $(document).ready(function(){
     "height":"50px",
     "margin-top": 0.90 * h + "px"});
 
-  setInterval(animateBall, 50);
-  setInterval(accelerateBall, 50);
+  setInterval(animateBall, updateInterval);
+  setInterval(accelerateBall, updateInterval);
 });
 
 var verticalVelocity = -20;
@@ -24,6 +25,7 @@ var accelerateBall = function(){
   verticalVelocity = verticalVelocity + 0.5;
 }
 
+//function that changes the css position of the ball every "updateInterval" milliseconds
 var animateBall = function(){
   $('#ball').css({
     //number of pixels the ball moves horizontally every 50 miliseconds
