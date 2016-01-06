a200b9eb2b1ab33ebda99401a16674d36b655ebd
Kartik <kartiktv97@gmail.com>
2016-01-06 14:03:18 -0800

    Added path data

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var w = window.innerWidth;
 var h = window.innerHeight;
 var updateInterval = 50;
 var verticalVelocity = -20;
+var horizontalVelocity = 5;
 
 $(document).ready(function(){
   var canvas = $('#canvas');
-
+  var ballData = $('#ball-data');
   //Creation of the ball element
   canvas.append("<div id = 'ball'></div>");
 
@@ -14,12 +15,23 @@ $(document).ready(function(){
     "border-radius":"50%",
     "width":"50px",
     "height":"50px",
-    "margin-top": 0.90 * h + "px"});
+    "margin-top": 0.75 * h + "px",
+    "z-index": "1"});
 
   setInterval(animateBall, updateInterval);
   setInterval(accelerateBall, updateInterval);
+  setInterval(changeData, updateInterval);
 });
 
+//used to update the data of the ball
+var changeData = function(){
+  var ballData = $('#ball-data');
+  ballData.empty();
+  ballData.append("<div id = 'container'>BALL DATA<br>Vertical Velocity: " + verticalVelocity
+                  + "<br>Horizontal velocity: " + horizontalVelocity + "</div>");
+}
+
+//used to update the velocity of the ball
 var accelerateBall = function(){
   verticalVelocity = verticalVelocity + 0.5;
 }
@@ -28,18 +40,19 @@ var accelerateBall = function(){
 var animateBall = function(){
 
   //bounce the ball if it reaches the bottom of the screen
-  if(h - findCurHeight() <= 0.1 * h){
+  if(h - findCurHeight() <= 0.25 * h){
     verticalVelocity = -20;
   }
 
   $('#ball').css({
     //number of pixels the ball moves horizontally every 50 miliseconds
-    "margin-left": '+=10px',
+    "margin-left": '+=' + horizontalVelocity + 'px',
     //number of pixels the ball moves vertically every 50 miliseconds
     "margin-top": '+=' + verticalVelocity + 'px'
   });
 }
 
+//function used to find the current height of the ball
 var findCurHeight = function(){
   var height = $('#ball').css("margin-top");
   length = height.length;
