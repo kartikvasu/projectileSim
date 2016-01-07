31f44d8bf655791f38870c4d5c44e7d5a7893d89
Kartik <kartiktv97@gmail.com>
2016-01-06 18:49:19 -0800

    Implemented using top and left instead of margins

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 var w = window.innerWidth;
 var h = window.innerHeight;
 
-var constHorizVelocity = 10;
-var constVertVelocity = -25;
+var constHorizVelocity = 1.75;
+var constVertVelocity = -1.75;
 
-var updateInterval = 50;
+var updateInterval = 5;
 var verticalVelocity = constVertVelocity;
 var horizontalVelocity = constHorizVelocity;
-var verticalAcceleration = 0.5;
+var verticalAcceleration = 0.005;
 
 $(document).ready(function(){
   var canvas = $('#canvas');
   var ballData = $('#ball-data');
   //Creation of the ball element
   canvas.append("<div id = 'ball'></div>");
-
+  canvas.css({
+    "height" : 0.75 * h + "px"
+  });
   var ball = $('#ball');
   ball.css({"background-color":"red",
     "border-radius":"50%",
     "width":"50px",
     "height":"50px",
-    "margin-top": 0.75 * h + "px",
-    "z-index": "1"});
+    //"margin-top": 0.75 * h + "px",
+    "z-index": "5"});
 
-  setInterval(animateBall, updateInterval);
-  setInterval(accelerateBall, updateInterval);
-  setInterval(changeData, updateInterval);
+    ball.offset({top: $('#canvas').height(), left: 0});
+
+  ball.click(function(){
+    setInterval(animateBall, updateInterval);
+    setInterval(accelerateBall, updateInterval);
+    setInterval(changeData, updateInterval);
+  })
 });
 
 //used to update the data of the ball
@@ -43,41 +49,60 @@ var accelerateBall = function(){
 
 //function that changes the css position of the ball every "updateInterval" milliseconds
 var animateBall = function(){
-
   //bounce the ball if it reaches the bottom of the screen
-  if(h - findCurHeight() <= 0.25 * h){
+  var h = $('#canvas').height();
+
+  if(findCurHeight() >= h){
     verticalVelocity = constVertVelocity;
   }
 
+
+  var w = $('#canvas').width();
   //bouncing off the wall if it reaches the right end
-  if(w - findCurDist() < 0.05 * w){
+  if(findCurDist() >= w){
     horizontalVelocity = 0 - constHorizVelocity;
   }
 
-  if(findCurDist() < 0.005 * w){
+  if(findCurDist() < 0.0005 * w){
     horizontalVelocity = constHorizVelocity;
   }
 
+  /*
   $('#ball').css({
     //number of pixels the ball moves horizontally every 50 miliseconds
     "margin-left": '+=' + horizontalVelocity + 'px',
     //number of pixels the ball moves vertically every 50 miliseconds
     "margin-top": '+=' + verticalVelocity + 'px'
   });
+  */
+
+  var x = $('#ball').offset();
+  $('#ball').offset({top: x.top + verticalVelocity,
+    left: x.left + horizontalVelocity});
+
 }
 
 //function used to find the current height of the ball
 var findCurHeight = function(){
+
+  /*
   var height = $('#ball').css("margin-top");
   length = height.length;
   height = height.slice(0, length - 2);
   return height;
+  */
+  var x = $('#ball').offset();
+  return x.top;
 }
 
 //function used to find the distance of the ball from the left wall
 var findCurDist = function(){
+  /*
   var dist = $('#ball').css("margin-left");
   length = dist.length;
   dist = dist.slice(0, length - 2);
   return dist;
+  */
+  var x = $('#ball').offset();
+  return x.left;
 }
